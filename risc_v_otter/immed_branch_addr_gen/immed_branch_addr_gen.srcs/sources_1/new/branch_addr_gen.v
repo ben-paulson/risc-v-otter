32eca99e7a66b74e8a8f67689fa857419d5edea7
Ben Paulson <30847493+ben-paulson@users.noreply.github.com>
2020-10-07 16:14:21 -1000

    Add comments

--- risc_v_otter/immed_branch_addr_gen/immed_branch_addr_gen.srcs/sources_1/new/branch_addr_gen.v
+++ risc_v_otter/immed_branch_addr_gen/immed_branch_addr_gen.srcs/sources_1/new/branch_addr_gen.v
@@ -1,5 +1,10 @@
 `timescale 1ns / 1ps
 
+/*
+  Generates new PC addresses for jal, jalr, and branch
+  instructions given the immediate values for I, J, B-type
+  instructions, and the current PC value.
+*/
 module branch_addr_gen(
     input [31:0] I_type, J_type, B_type, pc,
     output [31:0] jal, jalr, branch
@@ -7,6 +12,7 @@ module branch_addr_gen(
     
     parameter rs = 32'h0000_000C; // Source reg for jalr, hardcoded for now
     
+    // Assign addresses
     assign jal = pc + J_type;
     assign jalr = rs + I_type;
     assign branch = pc + B_type;
--- risc_v_otter/immed_branch_addr_gen/immed_branch_addr_gen.srcs/sources_1/new/immed_gen.v
+++ risc_v_otter/immed_branch_addr_gen/immed_branch_addr_gen.srcs/sources_1/new/immed_gen.v
@@ -1,10 +1,16 @@
 `timescale 1ns / 1ps
 
+/*
+  Generates immediate values given an
+  instruction input. Handles 5 instruction types
+  (the 5 that encode immediate values)
+*/
 module immed_gen(
     input [31:7] ir,
     output [31:0] U_type, I_type, S_type, J_type, B_type
     );
     
+    // Switch bits around according to appropriate instruction type
     assign U_type = {ir[31:12], 12'b0};
     assign I_type = {{21{ir[31]}}, ir[30:25], ir[24:20]};
     assign S_type = {{21{ir[31]}}, ir[30:25], ir[11:7]};
